51c3aa9608ba50544c55a594b68f7c991f0c72ac
BANIKKURAISHI <mdkuraishirahmanbanik@gmail.com>
2023-10-09 21:36:47 +0600

    I remove my reloding problem

--- src/Components/PROVIDER/AuthProvider.jsx
+++ src/Components/PROVIDER/AuthProvider.jsx
@@ -1,53 +1,67 @@
 import { createContext, useEffect, useState } from "react";
-import { PropTypes } from 'prop-types';
-import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { PropTypes } from "prop-types";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import auth from "../FireBase/Firebase.config";
 
- export const AuthContext=createContext(null)
-const AuthProvider = ({children}) => {
-    const [user,setUser]=useState(null)
-    const [loading,setLoading]=useState(true)
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const googleProvider = new GoogleAuthProvider()
-//--------Log in with google ------------------------------------------------------------------
-    const googleButton=()=>{
-               setLoading(true)
-                return signInWithPopup(auth, googleProvider)}
-                
-//---------------------------registration with email and password------------------------------
-const registrationButton=(email,password)=>{
-    setLoading(true)
-    return createUserWithEmailAndPassword(auth, email, password)
-}
-//---------------------------Login with email and password ------------------------------------
- const singInButton=(email,password)=>{
-    setLoading(true)
-    return signInWithEmailAndPassword(auth, email, password)
- }
- ////--------------------------onAuth ----------------------------------------------------------
- useEffect(()=>{
+  const googleProvider = new GoogleAuthProvider();
+  //--------Log in with google ------------------------------------------------------------------
+  const googleButton = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //---------------------------registration with email and password------------------------------
+  const registrationButton = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  //---------------------------Login with email and password ------------------------------------
+  const singInButton = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  ////--------------------------onAuth ----------------------------------------------------------
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
     
-     const unSubscribe =onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser)})
-        setLoading(false)
-        return()=>{unSubscribe()}
- },[])
- ///-----------------------logout ---------------------------------------------------------------
- const logOut=()=>{
-    setLoading(true)
-    return signOut(auth)
- }
-  
-//--------------------props ----------------------------------------------------------------------
-    const value={user,googleButton, registrationButton,singInButton, logOut,loading} 
-    return (
-        <AuthContext.Provider value={value}  >
-            {children}
-        </AuthContext.Provider>
-    );
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+  ///-----------------------logout ---------------------------------------------------------------
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //--------------------props ----------------------------------------------------------------------
+  const value = {
+    user,
+    googleButton,
+    registrationButton,
+    singInButton,
+    logOut,
+    loading,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-AuthProvider.propTypes={
-    children:PropTypes.node
-}
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
 export default AuthProvider;
--- src/Components/PROVIDER/Private.jsx
+++ src/Components/PROVIDER/Private.jsx
@@ -2,20 +2,20 @@
 import { PropTypes } from 'prop-types';
 import { useContext } from 'react';
 import { AuthContext } from './AuthProvider';
-import { Navigate,  } from 'react-router-dom';
+import { Navigate, useLocation,  } from 'react-router-dom';
 
 const Private = ({children}) => {
-    const{user}=useContext(AuthContext)
-  // const location =useLocation()
-    // if(loading){
-    //     return (<span className="loading loading-dots loading-xs"></span>
-    //     )
-    // }
-    //console.log(location)
-  //  if(loading){return <span className="loading loading-spinner loading-lg"></span>}
-    if(user){return children}
-    return (
-        <Navigate to="/in"></Navigate>
+    const{user,loading}=useContext(AuthContext)
+          const location =useLocation()
+            if(loading){
+              return (<span className="loading loading-dots loading-xs"></span>
+                 )
+              }
+            console.log(location)
+             if(loading){return <span className="loading loading-spinner loading-lg"></span>}
+             if(user){return children}
+               return (
+            <Navigate state={location.pathname}  to="/in"></Navigate>
     );
 }
 
